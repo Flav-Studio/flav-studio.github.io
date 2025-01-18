684bf5da9649dff0b6eb98e41c0ddfcaa5099ed4
ShortLegsFox <mister.dusbin777@gmail.com>
2025-01-18 14:49:49 +0100

    wip: starting the landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1>Acceuil</h1>
+    <div class="flex flex-col items-center mx-auto text-[125px] h-[100vh] max-w-max">
+        <div class="flex">
+            <img src="" alt="">
+            <p>Creative</p>
+            <img src="" alt="">
+        </div>
+        <div class="flex">
+            <img src="" alt="">
+            <p>Minds</p>
+            <p class="text-[24px]">* Graphiste & Illustratrice</p>
+        </div>
+        <div class="flex">
+            <img src="" alt="">
+            <p>Never</p>
+        </div>
+        <div class="flex">
+            <p>Sleep</p>
+            <img src="" alt="">
+        </div>     
+    </div>
+    <div class="flex flex-col items-center text-[24px] h-[100vh] mx-auto max-w-max">
+        <div class="text-center w-[570px]">
+            <p class="text-[50px] pb-[29px]">Hello !</p>
+            <p class="pb-[29px]">Moi c'est Flavie, et je suis en Master 1 de design graphique et digital à Strasbourg.</p>
+            <p class="pb-[29px]">Artiste/graphiste pluridisciplinaire j'explore aussi bien la peinture, l'illustration, la sculpture, que la photographie, le graphisme, et le web...</p>
+            <p class="pb-[29px]">En dehors de mes créations, je suis une grande amatrice de café, de chocolat, et une amie des animaux !</p>
+        </div>
+        <div class="flex">
+        </div>
+        <div class="flex">
+        </div>
+        <div class="flex">
+        </div>     
+    </div>
 </template>
