c15652d0a420e92bb7ef35728b9e674504e24613
Ian BELLOT <ian.bellot@al-enterprise.com>
2025-02-07 13:32:26 +0100

    feat: starting responsive design on index

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,35 @@
 <template>
-    <div class="flex flex-col items-center mx-auto text-[125px] text-fsblack max-w-max">
-        <div class="flex items-center gap-11 pt-8">
-            <img class="h-44" src="~/assets/img/home-1.jpg" alt="home image 1">
-            <p class="font-helvetica text-[12rem] leading-none">Creative</p>
-            <img class="h-44" src="~/assets/img/home-2.jpg" alt="home image 2">
+  <div class="px-6 md:px-16 lg:px-28">
+    <div class="max-w-max mx-auto rounded-t-[25px] w-full overflow-hidden">
+      <div class="flex flex-col items-center mx-auto text-fsblack">
+        <div class="flex justify-center items-center gap-[5vw] md:gap-[37px] pt-2 max-w-full">
+            <img class="h-[12vw] md:h-[10vw]" src="~/assets/img/home-1.jpg" alt="home image 1">
+            <p class="font-helvetica text-[10vw] md:text-[12vw] leading-none">Creative</p>
+            <img class="h-[12vw] md:h-[10vw]" src="~/assets/img/home-2.jpg" alt="home image 2">
         </div>
-        <div class="flex items-center gap-11 mt-[-20px] self-end mr-8">
-            <img class="h-44" src="~/assets/img/home-3.jpg" alt="home image 3">
-            <p class="font-PPPangaia italic text-[12rem] leading-none">Minds</p>
-            <p class="text-[24px] font-lucida w-[200px]">* Graphiste & Illustratrice</p>
+          <div class="flex items-center gap-11 mt-[-20px] self-end mr-8">
+              <img class="h-44" src="~/assets/img/home-3.jpg" alt="home image 3">
+              <p class="font-PPPangaia italic text-[12rem] leading-none">Minds</p>
+              <p class="text-[24px] font-lucida w-[200px]">* Graphiste & Illustratrice</p>
+          </div>
+        <div class="flex gap-11">
+          <div class="mt-[-20px]">
+            <div class="flex items-center gap-5">
+              <img class="h-[177px]" src="~/assets/img/home-5.jpg" alt="home image 5">
+              <p class="font-helvetica text-[12rem] leading-none">Never</p>
+            </div>
+            <div class="flex">
+              <p class="font-PPPangaia italic text-[12rem] leading-none">Sleep</p>
+              <img class="h-[262px]" src="~/assets/img/home-6.png" alt="">
+            </div>
+          </div>
+          <div class="flex">
+            <img class="h-[431px]" src="~/assets/img/home-4.jpg" alt="home image 4">
+          </div>
         </div>
-       <div class="flex gap-11">
-         <div class="mt-[-20px]">
-           <div class="flex items-center gap-5">
-             <img class="h-[177px]" src="~/assets/img/home-5.jpg" alt="home image 5">
-             <p class="font-helvetica text-[12rem] leading-none">Never</p>
-           </div>
-           <div class="flex">
-             <p class="font-PPPangaia italic text-[12rem] leading-none">Sleep</p>
-             <img class="h-[262px]" src="~/assets/img/home-6.png" alt="">
-           </div>
-         </div>
-         <div class="flex">
-           <img class="h-[431px]" src="~/assets/img/home-4.jpg" alt="home image 4">
-         </div>
-       </div>
+      </div>
     </div>
+  </div>
 </template>
 <script setup lang="ts">
 </script>
