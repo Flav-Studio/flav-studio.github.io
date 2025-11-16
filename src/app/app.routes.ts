bbdc3b56e241949d3f21dc693b37fb1ee568daea
ShortLegsFox <mister.dusbin777@gmail.com>
2025-11-16 14:42:47 +0100

    rectif on menu page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Menu } from './components/menu/menu';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [
-    { path: '', component: Menu }
+    { path : '', component: Home },
+    { path: 'menu', component: Menu }
 ];
--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
